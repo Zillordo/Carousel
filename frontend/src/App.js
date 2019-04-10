0bf56eb91aa6226efd5cbcbe7bd70c6e884adf6c
Zillordo <konecnyallan@gmail.com>
2019-04-10 14:29:22 +0200

    bakc-end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 
+import './style/style.css';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hey
+        
       </div>
     );
   }
