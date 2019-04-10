0d09ac1e7526be4797a0d1f51195dde618a0ff0b
Zillordo <konecnyallan@gmail.com>
2019-04-10 13:55:30 +0200

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Hey
+      </div>
+    );
+  }
+}
+
+export default App;
