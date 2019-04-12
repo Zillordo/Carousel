d25c4f13ddfab60ca0db42b85437940bc0084475
Zillordo <konecnyallan@gmail.com>
2019-04-12 15:44:18 +0200

    Adding apollo

--- frontend/src/components/settingsmodal.js
+++ frontend/src/components/settingsmodal.js
@@ -1,6 +1,24 @@
-import React, { useState } from 'react';
-import Reorder, { reorder } from 'react-reorder';
+import React, { Fragment, useState } from 'react';
+// import Reorder, { reorder } from 'react-reorder';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider, Query } from 'react-apollo';
+import gql from 'graphql-tag';
 
+
+const TILES_QUERY = gql`
+    query TilesQuery{
+        tiles {
+            id
+            subHeader
+            heading
+            positive
+            background
+  }
+}
+`;
+const client = new ApolloClient({
+    uri: 'http://localhost:4000/graphql'
+});
 
 const Tile = ({ subheader, ...props }) => {
 
@@ -8,77 +26,90 @@ const Tile = ({ subheader, ...props }) => {
 
 
     return (
-        <React.Fragment>
-            <div className="tile">
-                <div className="subheader">
-                    <div className="dnd"></div>
-                    <input type="text" value={subheader} />
-                </div>
-                <div className="heading">
-                    <input type="text"></input>
-                </div>
-                <div className="positive">
-                    <button onClick={() => setSlider(!slider)} style={slider ? { backgroundColor: '#dadada' } : { backgroundColor: '#9700fd' }}>
-                        <div className="slider" style={slider ? { float: 'left' } : { float: 'right' }}></div>
-                    </button>
-                </div>
+        <div className="tile">
+            <div className="subheader">
+                <div className="dnd"></div>
+                <input type="text" value={subheader} />
             </div>
-        </React.Fragment>
+            <div className="heading">
+                <input type="text"></input>
+            </div>
+            <div className="positive">
+                <button onClick={() => setSlider(!slider)} style={slider ? { backgroundColor: '#dadada' } : { backgroundColor: '#9700fd' }}>
+                    <div className="slider" style={slider ? { float: 'left' } : { float: 'right' }}></div>
+                </button>
+            </div>
+        </div>
     )
 }
 
 const Settings = ({ toggle }) => {
 
-    const [tile, setTile] = useState(["Photo", "Nature", "Car"]);
+    // const [tile, setTile] = useState(["Photo", "Nature", "Car"]);
 
 
-    const order = (event, previusIndex, nextIndex) => {
-        event.preventDefault();
-        setTile(() => reorder(tile, previusIndex, nextIndex));
-    }
+    // const order = (event, previusIndex, nextIndex) => {
+    //     event.preventDefault();
+    //     setTile(() => reorder(tile, previusIndex, nextIndex));
+    // }
 
     return (
-        <div className="settings">
-            <button className="settings--canclebtn" onClick={toggle}>X</button>
-            <div className="settings--header">
-                <h1>Header Settings</h1>
-                <div className="header--options">
-                    <div className="header--sizes">
-                        <p>Size</p>
-                        <button>S</button>
-                        <button>M</button>
-                        <button>L</button>
-                    </div>
-                    <div className="header--animation">
-                        <p>Animations</p>
-                        <select>
-                            <option>Frtine wheel</option>
-                        </select>
-                        <select>
-                            <option>Auto 2 sec</option>
-                        </select>
+        <ApolloProvider client={client}>
+            <div className="settings">
+                <button className="settings--canclebtn" onClick={toggle}>X</button>
+                <div className="settings--header">
+                    <h1>Header Settings</h1>
+                    <div className="header--options">
+                        <div className="header--sizes">
+                            <p>Size</p>
+                            <button>S</button>
+                            <button>M</button>
+                            <button>L</button>
+                        </div>
+                        <div className="header--animation">
+                            <p>Animations</p>
+                            <select>
+                                <option>Frtine wheel</option>
+                            </select>
+                            <select>
+                                <option>Auto 2 sec</option>
+                            </select>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className="settings--body">
-                <div className="body-add">Tiles</div>
-                <div className="body--tiles">
-                    <p>SUBHEADER</p><p>HEADING</p><p>POSITIVE</p><p>BACKGROUND</p>
-                    <Reorder reorderId="my-list" autoScroll={true} className="list" onReorder={order} holdTime={150}>
-                        {
-                            tile.map((item) => (
-                                <div key={item}>
-                                    <Tile subheader={item}/>
-                                </div>
-                            ))
-                        }
-                    </Reorder>
+                <div className="settings--body">
+                    <div className="body-add">Tiles</div>
+                    <div className="body--tiles">
+                        <p>SUBHEADER</p><p>HEADING</p><p>POSITIVE</p><p>BACKGROUND</p>
+                        <Query query={TILES_QUERY}>
+                            {
+                                ({ loading, error, data }) => {
+                                    if (loading) return <h4>Loading</h4>
+                                    if (error) console.log(error)
+                                    console.log(data);
+                                    return(  
+                                    <Fragment>
+                                        {
+                                            data.tiles.map(tile => (
+                                                <div className="list" key={tile.id}>
+                                                    <Tile
+                                                        subheader={tile.subHeader}
+                                                    />
+                                                </div>
+                                            ))
+                                        }
+                                    </Fragment>
+                                    )
+                                }
+                            }
+                        </Query>
+                    </div>
+                </div>
+                <div className="settings--footer">
+                    <button>SAVE</button>
                 </div>
             </div>
-            <div className="settings--footer">
-                <button>SAVE</button>
-            </div>
-        </div>
+        </ApolloProvider>
     )
 }
 
--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const expressGql = require('express-graphql');
+const cors = require('cors');
 
 const schema = require('./graphql/schema');
 
 const app = express();
+
+app.use(cors())
 
 app.use('/graphql', expressGql({
     schema: schema,
